f1c3a88ec5a9c59a6f0ae50aaf63d7aedaa234cb
Shelwin Xiao <562687381@qq.com>
2019-11-05 14:37:17 +0800

    incease case for missing lines

--- src/test/java/io/jenkins/plugins/pipelinemonitor/StageStatusListenerTest.java
+++ src/test/java/io/jenkins/plugins/pipelinemonitor/StageStatusListenerTest.java
@@ -11,9 +11,22 @@ import io.jenkins.plugins.pipelinemonitor.StageStatusListener;
 
 import static org.junit.Assert.assertEquals;
 
+import org.jenkinsci.plugins.pipeline.StageStatus;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({StageStatus.class})
 public class StageStatusListenerTest {
+  @Before
+  public void setUp() {
+    PowerMockito.mockStatic(StageStatus.class);
+  }
+
   @Test
-  public void testBuildStateForStageWithNullTag() {
+  public void testBuildStateForStageWithNullErrorAction() throws Exception {
     StepStartNode flowNodeMock = mock(StepStartNode.class);
     // ErrorAction errorActionMock = mock(ErrorAction.class);
     when(flowNodeMock.getAction(TagsAction.class)).thenReturn(null);
@@ -22,5 +35,44 @@ public class StageStatusListenerTest {
     assertEquals(state, "CompletedSuccess");
   }
 
+  @Test
+  public void testBuildStateForStageWithErrorAction() throws Exception {
+    StepStartNode flowNodeMock = mock(StepStartNode.class);
+    ErrorAction errorActionMock = mock(ErrorAction.class);
+    when(flowNodeMock.getAction(TagsAction.class)).thenReturn(null);
 
+    String state = StageStatusListener.buildStateForStage(flowNodeMock, errorActionMock);
+    assertEquals(state, "CompletedError");
+  }
+
+  @Test
+  public void testBuildStateForStageWithTags() throws Exception {
+    StepStartNode flowNodeMock = mock(StepStartNode.class);
+    ErrorAction errorActionMock = mock(ErrorAction.class);
+    TagsAction tagsActionMock = mock(TagsAction.class);
+    when(flowNodeMock.getAction(TagsAction.class)).thenReturn(tagsActionMock);
+
+    when(tagsActionMock.getTagValue(StageStatus.TAG_NAME)).thenReturn("SKIPPED_FOR_FAILURE");
+    PowerMockito.when(StageStatus.class, "getSkippedForFailure").thenReturn("SKIPPED_FOR_FAILURE");
+    String stateSkipped = StageStatusListener.buildStateForStage(flowNodeMock, errorActionMock);
+    assertEquals(stateSkipped, "SkippedFailure");
+
+    when(tagsActionMock.getTagValue(StageStatus.TAG_NAME)).thenReturn("SKIPPED_FOR_UNSTABLE");
+    PowerMockito.when(StageStatus.class, "getSkippedForUnstable")
+        .thenReturn("SKIPPED_FOR_UNSTABLE");
+    String stateUnstable = StageStatusListener.buildStateForStage(flowNodeMock, errorActionMock);
+    assertEquals(stateUnstable, "SkippedUnstable");
+
+    when(tagsActionMock.getTagValue(StageStatus.TAG_NAME)).thenReturn("SKIPPED_FOR_CONDITIONAL");
+    PowerMockito.when(StageStatus.class, "getSkippedForConditional")
+        .thenReturn("SKIPPED_FOR_CONDITIONAL");
+    String stateSkipCon = StageStatusListener.buildStateForStage(flowNodeMock, errorActionMock);
+    assertEquals(stateSkipCon, "SkippedConditional");
+
+    when(tagsActionMock.getTagValue(StageStatus.TAG_NAME)).thenReturn("FAILED_AND_CONTINUED");
+    PowerMockito.when(StageStatus.class, "getFailedAndContinued")
+        .thenReturn("FAILED_AND_CONTINUED");
+    String stateFail = StageStatusListener.buildStateForStage(flowNodeMock, errorActionMock);
+    assertEquals(stateFail, "CompletedError");
+  }
 }
